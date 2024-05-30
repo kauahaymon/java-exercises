8ed47128ef8c398497c2a32284ea07d5e4092c57
Kauã Haymon <kauahaymon@hotmail.com>
2024-05-30 19:43:44 -0400

    Comparing objects using hashCode and Equals

---
+++ hashCode-and-Equals/entities/Client.java
@@ -0,0 +1,52 @@
+package entities;
+
+import java.util.Objects;
+
+public class Client {
+    private String name;
+    private String email;
+
+    public Client(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public Client() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /*
+    hashCode and equals
+     */
+
+    // Override equals method to compare Client objects
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return Objects.equals(name, client.name) && Objects.equals(email, client.email);
+    }
+
+    // Override hashCode method to generate hash code for Client objects
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, email);
+    }
+}
